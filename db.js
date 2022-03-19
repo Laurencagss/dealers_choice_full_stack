eca4a4165db7c05353944af754e8dcf68430f4a3
Lauren Cagnetta <laurenscolleges@gmail.com>
2022-03-19 11:58:19 -0400

    second try

--- db.js
+++ db.js
@@ -1,28 +1,24 @@
 const { bindActionCreators } = require('redux');
 const Sequelize = require('sequelize');
-const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme-react-redux');
+const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/dealers-choice-full-stack');
 
-const Friends = db.define('friends', {
+const Things = db.define('things', {
     name: {
         type: Sequelize.STRING,
         allowNull: false
-    },
-    pronouns: {
-        type: Sequelize.STRING,
-    },
-    origin: {
-        type: Sequelize.STRING,
-    }
+}
 });
 
 const syncAndSeed = async() => {
     await db.sync ({ force: true });
-    await Friends.create({ name: 'Jenna', pronouns: 'she/her', origin: 'Girlfriend' });
-    await Friends.create({ name: 'KT', pronouns: 'she/her', origin: 'Roomie' });
-    await Friends.create({ name: 'Rohail', pronouns: 'he/him', origin: 'College' });
-    await Friends.create({ name: 'Ray', pronouns: ('she/her', 'they/them'), origin: 'Sister' });
+    await Things.create({ name: 'pompadour' });
+    await Things.create({ name: 'shaved-head' });
+    await Things.create({ name: 'leg-hair' });
+    await Things.create({ name: 'patches' });
+    await Things.create({ name: 'mismatch' });
+
    return {
-        friend: ['Jenna', 'KT', 'Rohail', 'Ray']
+        thing: ['pompadour', 'shaved-head', 'leg-hair', 'mismatch', 'patches']
    }
 }
 
@@ -30,6 +26,6 @@ module.exports = {
     syncAndSeed, 
     db,
     models: {
-        Friend
+       Thing
     }
     }
